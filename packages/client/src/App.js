df3737f5e881573d5404c03f896d0d8d6ede10f9
davidchak <davidchak@yandex.ru>
2022-08-22 21:56:20 +0300

    update to webpack 5

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import withEventBus from "./events"
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
+import ComponentC from "./components/ComponentC";
+import ComponentD from "./components/ComponentD";
 
-function App() {
+const App = () => {
+  const arr = new Array(100);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <ComponentB />
+      <ComponentA id={1} />
+      <ComponentA id={2}  />
+      <ComponentA id={3}  />
+      <ComponentA id={4}  />
+      <ComponentA id={5}  />
+      <ComponentA id={6}  />
+      <ComponentA id={7}  />
+      <ComponentA id={8}  />
+      <ComponentA id={9}  />
+      <ComponentA id={10} />
+      {/* <ComponentC /> */}
+      {/* <ComponentD /> */}
     </div>
   );
-}
+};
 
-export default App;
+export default withEventBus(App);
