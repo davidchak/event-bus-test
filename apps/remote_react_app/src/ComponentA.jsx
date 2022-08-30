750fb8e58e6fdebfc4ba9578abe5c2bc53d1ac4a
davidchak <davidchak@yandex.ru>
2022-08-30 23:24:21 +0300

    update

--- apps/remote_react_app/src/ComponentA.jsx
+++ apps/remote_react_app/src/ComponentA.jsx
@@ -1,41 +1,41 @@
 import { useEffect, useRef, useState } from "react";
 
-// const ComponentA = () => {
-//     const msgBus = useRef(null);
-//     const [activate, setActivate ] = useState(true)
-//     const [busEvent, setBusEvent ] = useState(null)
+const ComponentA = () => {
+    const msgBus = useRef(null);
+    const [activate, setActivate ] = useState(true)
+    const [busEvent, setBusEvent ] = useState(null)
 
-//     const handleClick = () => {
-//         setActivate(!activate);
-//     };
+    const handleClick = () => {
+        setActivate(!activate);
+    };
 
-//     useEffect(() => {
+    useEffect(() => {
 
-//         const messageEvent = window.sdk.message;
+        const messageEvent = window.sdk.message;
 
-//         if(!messageEvent){
-//             console.log('error')
-//         }
+        if(!messageEvent){
+            console.log('error')
+        }
 
-//         if(activate){
-//             msgBus.current = messageEvent.subscribe((data) => { 
-//                 setBusEvent(data)
-//             })
-//         }
+        if(activate){
+            msgBus.current = messageEvent.subscribe((data) => { 
+                setBusEvent(data)
+            })
+        }
 
-//         if(!activate && msgBus.current){
-//             msgBus.current.unsubscribe()
-//             setBusEvent(null)
-//         }
-//     }, [activate]);
+        if(!activate && msgBus.current){
+            msgBus.current.unsubscribe()
+            setBusEvent(null)
+        }
+    }, [activate]);
 
-//     return <div className="component_x">
-//         <span>Remote react component</span>
-//         <div>{busEvent}</div>
-//         <button onClick={handleClick}>{activate ? 'on' : 'off'}</button>
-//     </div>;
-// };
+    return <div className="component_x">
+        <span>Remote react component</span>
+        <div>{busEvent}</div>
+        <button onClick={handleClick}>{activate ? 'on' : 'off'}</button>
+    </div>;
+};
 
-const ComponentA = () => <div>Remote react component</div>
+// const ComponentA = () => <div>Remote react component</div>
 
 export default ComponentA
